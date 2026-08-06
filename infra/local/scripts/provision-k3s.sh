43467082001fdd1364efd1e1c341b4a2b6ad3cd3
simontesar <simon@simontesar.com>
2026-08-06 06:29:46 +0200

    infra: local: convert to libvirt Signed-off-by: simontesar <simon@simontesar.com>

--- infra/local/scripts/provision-k3s.sh
+++ infra/local/scripts/provision-k3s.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 ENV_DIR="$(pwd)"
 
-MASTER_IP=$(
-  vagrant ssh-config master | awk '/HostName/ { print $2 }'
-)
+SSH_CONFIG=$(vagrant ssh-config master)
+MASTER_IP=$(awk '/HostName/ { print $2 }' <<<"${SSH_CONFIG}")
+MASTER_KEY=$(awk '/IdentityFile/ { print $2 }' <<<"${SSH_CONFIG}")
 
-if [[ -z "${MASTER_IP}" ]]; then
-  echo "Could not determine master IP from vagrant ssh-config" >&2
+if [[ -z "${MASTER_IP}" || -z "${MASTER_KEY}" ]]; then
+  echo "Could not determine master IP/private key from vagrant ssh-config" >&2
   exit 1
 fi
 
 cat > inventory <<EOF
 [master]
-master ansible_host=${MASTER_IP} ansible_user=vagrant ansible_private_key_file=.vagrant/machines/master/parallels/private_key
+master ansible_host=${MASTER_IP} ansible_user=vagrant ansible_private_key_file=${MASTER_KEY}
 
 [master:vars]
 ansible_ssh_common_args=-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
